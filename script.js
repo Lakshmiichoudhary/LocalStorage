fe833310be46946b9e2d6b690b22e6f027ea358b
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-08-28 15:56:39 +0530

    added delete option

--- script.js
+++ script.js
@@ -6,31 +6,45 @@ const usersList = document.getElementById('users');
 // Load existing users from local storage
 const savedUsers = JSON.parse(localStorage.getItem('users')) || [];
 
-// Display existing users 
-savedUsers.forEach(user => {
-    const li = document.createElement('li');
-    li.textContent = `${user.name} - ${user.email}`;
-    usersList.appendChild(li);
-});
+// Function to display existing users
+function displayUsers() {
+    usersList.innerHTML = ""; // Clear the list before re-adding
+    savedUsers.forEach((user, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${user.name} - ${user.email}`;
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener('click', () => deleteUser(index));
+        
+        li.appendChild(deleteButton);
+        usersList.appendChild(li);
+    });
+}
+
+// Function to delete a user
+function deleteUser(index) {
+    savedUsers.splice(index, 1); 
+    localStorage.setItem('users', JSON.stringify(savedUsers)); 
+    displayUsers(); 
+}
+
+// Display existing users on page load
+displayUsers();
 
 // Add event listener 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    // Get user input
+    
     const name = nameInput.value;
     const email = emailInput.value;
-
-    // Store user details in local storage
+    
     const user = { name, email };
     savedUsers.push(user);
     localStorage.setItem('users', JSON.stringify(savedUsers));
-
-    const li = document.createElement('li');
-    li.textContent = `${name} - ${email}`;
-    usersList.appendChild(li);
-
-    // Clear the form inputs
+    
+    displayUsers();
+    
     nameInput.value = '';
     emailInput.value = '';
 });
